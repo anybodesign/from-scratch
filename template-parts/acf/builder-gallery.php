898442217296b6548e5ee00a44b317b38b06bdc7
thomas <atelier@anybodesign.com>
2018-04-15 23:49:31 +0200

    Builder CSS #2

--- template-parts/acf/builder-gallery.php
+++ template-parts/acf/builder-gallery.php
@@ -8,5 +8,40 @@
 ?>
 
 					<section class="builder-gallery">
-						<p>Module Galerie photos</p>
+						<?php 
+							$intro = get_sub_field('intro');
+							$text = get_sub_field('text');
+							$cols = get_sub_field('columns');
+							$images = get_sub_field('gallery');
+						?>
+				
+						<?php if( $text ) { ?>
+						<div class="builder-gallery-text">
+							<?php echo $text; ?>
+						</div>
+						<?php } ?>
+						
+						<?php if( $images ): ?>
+						<div class="builder-gallery-pictures">
+							
+					        <?php foreach( $images as $image ): ?>
+					        <div class="gallery-item-<?php echo $cols; ?>">
+						        
+						        <figure class="builder-gallery-figure">
+						            <a href="<?php echo $image['url']; ?>" title="<?php _e('Enlarge picture', 'from-scratch'); ?>">
+							            <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>">
+										<?php if ( $image['caption'] ) { ?>
+										<figcaption class="builder-gallery-caption">
+											<span class="builder-gallery-caption-title"><?php echo $image['caption']; ?></span>
+										</figcaption>
+										<?php } ?>
+							        </a>
+							    </figure>
+							    
+					        </div>
+					        <?php endforeach; ?>
+					        
+						</div>
+						<?php endif; ?>							
+		
 					</section>
