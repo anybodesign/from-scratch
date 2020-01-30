797e4eae73e599633f7783d281add5031194756a
Thomas Villain <atelier@anybodesign.com>
2020-01-30 08:40:12 +0100

    Title for search results

--- template-parts/page-banner.php
+++ template-parts/page-banner.php
@@ -19,6 +19,8 @@
 									single_post_title();
 								} else if ( is_archive() ) {
 									the_archive_title();
+								} else if ( is_search() ) {
+									printf( esc_html__( 'Search Results for: %s', 'from-scratch' ), '<span class="search-term">' . get_search_query() . '</span>' );
 								} else if ( is_404() ) {
 									esc_html_e( 'Oops! That page can&rsquo;t be found', 'from-scratch' );
 								} else {
