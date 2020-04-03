fe8f451d52dc2ce2a10189415ed0979ada49056d
Thomas Villain <atelier@anybodesign.com>
2020-04-03 13:30:22 +0200

    Template cleaning

--- page.php
+++ page.php
@@ -19,14 +19,7 @@ get_header(); ?>
 							get_template_part( 'template-parts/page', 'content' );
 						endwhile;					
 					?>
-					
-					
-					<?php // ACF Builder output ?>
-					
-					<div class="builder-content">
-						<?php get_template_part('template-parts/acf/builder'); ?>
-					</div>
-										
+
 				</div>
 				
 <?php get_footer(); ?>
