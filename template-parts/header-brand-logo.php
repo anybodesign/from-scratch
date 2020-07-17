030ce9919f26b38ce884c000bc69d3785f2c931e
Thomas Villain <atelier@anybodesign.com>
2020-07-17 15:46:21 +0200

    site name code fix

--- template-parts/header-brand-logo.php
+++ template-parts/header-brand-logo.php
@@ -17,4 +17,4 @@
 					<img class="logo<?php if ( get_theme_mod('site_logo_mobile') ) { echo esc_attr(' has-mobile-logo'); } ?>" src="<?php echo esc_url(get_theme_mod('site_logo', 'none')); ?>" alt="">
 					<?php } ?>
 					
-					<span<?php if (get_theme_mod( 'hide_sitetitle' )) { echo esc_html(' class="screen-reader-text"'); } ?>><?php echo esc_html(bloginfo('name')); ?>	</span>
+					<span class="site-name<?php if (get_theme_mod( 'hide_sitetitle' )) { echo esc_attr(' screen-reader-text'); } ?>"><?php echo esc_html(bloginfo('name')); ?></span>
