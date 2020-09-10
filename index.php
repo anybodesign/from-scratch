081d13debe9acf30e32f5d4e87a81f725f8eae1b
Thomas Villain <atelier@anybodesign.com>
2020-09-10 10:57:01 +0200

    IAS markup

--- index.php
+++ index.php
@@ -19,29 +19,23 @@ get_header(); ?>
 
 					<div class="page-content">
 					
-					<?php // The Loop ?>
-					
-					<?php if ( have_posts() ) : ?>		
-			
-						<?php while ( have_posts() ) : the_post(); ?>
-			
-							<?php get_template_part( 'template-parts/post', 'block' ); ?>
-			
-						<?php endwhile; ?>
-			
-						<?php the_posts_pagination(array(
-								'prev_text'          => __( 'Previous page', 'from-scratch' ),
-								'next_text'          => __( 'Next page', 'from-scratch' ),
-								'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'from-scratch' ) . ' </span>',
-							)); ?>
-			
-					<?php else : ?>
-	
-						<?php get_template_part( 'template-parts/nothing' ); ?>
-				
-					<?php endif; ?>	
+						<?php // The Loop ?>
+						<?php if ( have_posts() ) : ?>		
+						<div id="posts_list" class="the-posts" >
+							<?php 
+								while ( have_posts() ) : the_post();
+									get_template_part( 'template-parts/post', 'block' ); 
+								endwhile;
+							?>
+						</div>
 						
-	
+						<?php get_template_part( 'template-parts/post', 'pagination' ); ?>
+				
+						<?php
+						else:
+							get_template_part( 'template-parts/nothing' );
+						endif; 
+						?>
 					</div>
 
 
