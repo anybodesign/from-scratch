28a7f461cc91e42418ae06100c3e7a82770eb4ea
Thomas Villain <atelier@anybodesign.com>
2020-10-13 16:02:47 +0200

    CPT reloaded

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -9,7 +9,7 @@ class FS_CPT {
 		
 		add_action( 'init',                 array( $this, 'cpt_project'), 1 );
 		
-		add_action( 'init',                 array( $this, 'custom_taxonomy'), 1 );
+		add_action( 'init',                 array( $this, 'tax_project'), 1 );
 		
 		//add_action( 'pre_get_posts',		array( $this, 'pre_get_posts' ), 1 );
 
@@ -25,50 +25,49 @@ class FS_CPT {
 	*
 	**/
 	
-	
 	function cpt_project() {
-
+	
 		$labels = array(
 			'name'                  => _x( 'Projects', 'Post Type General Name', 'from-scratch' ),
 			'singular_name'         => _x( 'Project', 'Post Type Singular Name', 'from-scratch' ),
 			'menu_name'             => __( 'Projects', 'from-scratch' ),
-			'name_admin_bar'        => __( 'Project', 'from-scratch' ),
-			'archives'              => __( 'Project archives', 'from-scratch' ),
-			'attributes'            => __( 'Project attributes', 'from-scratch' ),
-			'parent_item_colon'     => __( 'Parent project:', 'from-scratch' ),
-			'all_items'             => __( 'All the projects', 'from-scratch' ),
-			'add_new_item'          => __( 'Add new project', 'from-scratch' ),
+			'name_admin_bar'        => __( 'Projects', 'from-scratch' ),
+			'archives'              => __( 'Item Archives', 'from-scratch' ),
+			'attributes'            => __( 'Item Attributes', 'from-scratch' ),
+			'parent_item_colon'     => __( 'Parent Item:', 'from-scratch' ),
+			'all_items'             => __( 'All Items', 'from-scratch' ),
+			'add_new_item'          => __( 'Add New Item', 'from-scratch' ),
 			'add_new'               => __( 'Add New', 'from-scratch' ),
-			'new_item'              => __( 'New project', 'from-scratch' ),
-			'edit_item'             => __( 'Edit project', 'from-scratch' ),
-			'update_item'           => __( 'Update project', 'from-scratch' ),
-			'view_item'             => __( 'View project', 'from-scratch' ),
-			'view_items'            => __( 'View projects', 'from-scratch' ),
-			'search_items'          => __( 'Search project', 'from-scratch' ),
+			'new_item'              => __( 'New Item', 'from-scratch' ),
+			'edit_item'             => __( 'Edit Item', 'from-scratch' ),
+			'update_item'           => __( 'Update Item', 'from-scratch' ),
+			'view_item'             => __( 'View Item', 'from-scratch' ),
+			'view_items'            => __( 'View Items', 'from-scratch' ),
+			'search_items'          => __( 'Search Item', 'from-scratch' ),
 			'not_found'             => __( 'Not found', 'from-scratch' ),
 			'not_found_in_trash'    => __( 'Not found in Trash', 'from-scratch' ),
-			'featured_image'        => __( 'Project picture', 'from-scratch' ),
-			'set_featured_image'    => __( 'Set project picture', 'from-scratch' ),
-			'remove_featured_image' => __( 'Remove project picture', 'from-scratch' ),
-			'use_featured_image'    => __( 'Use as project picture', 'from-scratch' ),
-			'insert_into_item'      => __( 'Insert into project', 'from-scratch' ),
-			'uploaded_to_this_item' => __( 'Uploaded to this project', 'from-scratch' ),
-			'items_list'            => __( 'Projects list', 'from-scratch' ),
-			'items_list_navigation' => __( 'Projects list navigation', 'from-scratch' ),
-			'filter_items_list'     => __( 'Filter events list', 'from-scratch' ),
+			'featured_image'        => __( 'Featured Image', 'from-scratch' ),
+			'set_featured_image'    => __( 'Set featured image', 'from-scratch' ),
+			'remove_featured_image' => __( 'Remove featured image', 'from-scratch' ),
+			'use_featured_image'    => __( 'Use as featured image', 'from-scratch' ),
+			'insert_into_item'      => __( 'Insert into item', 'from-scratch' ),
+			'uploaded_to_this_item' => __( 'Uploaded to this item', 'from-scratch' ),
+			'items_list'            => __( 'Items list', 'from-scratch' ),
+			'items_list_navigation' => __( 'Items list navigation', 'from-scratch' ),
+			'filter_items_list'     => __( 'Filter items list', 'from-scratch' ),
 		);
 		$rewrite = array(
-			'slug'                  => 'project',
+			'slug'                  => __('project', 'from-scratch'),
 			'with_front'            => true,
 			'pages'                 => true,
-			'feeds'                 => false,
+			'feeds'                 => true,
 		);
 		$args = array(
 			'label'                 => __( 'Project', 'from-scratch' ),
-			'description'           => __( 'Project description', 'from-scratch' ),
+			'description'           => __( 'Post Type Description', 'from-scratch' ),
 			'labels'                => $labels,
-			'supports'              => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
-			'taxonomies'            => array( 'project_type' ),
+			'supports'              => array( 'title', 'editor', 'thumbnail', 'comments', 'revisions', 'page-attributes' ),
+			'taxonomies'            => array( 'project-type' ),
 			'hierarchical'          => false,
 			'public'                => true,
 			'show_ui'               => true,
@@ -78,7 +77,7 @@ class FS_CPT {
 			'show_in_admin_bar'     => true,
 			'show_in_nav_menus'     => true,
 			'can_export'            => true,
-			'has_archive'           => 'projects',
+			'has_archive'           => true,
 			'exclude_from_search'   => false,
 			'publicly_queryable'    => true,
 			'rewrite'               => $rewrite,
@@ -86,9 +85,7 @@ class FS_CPT {
 			'show_in_rest'          => true,
 		);
 		register_post_type( 'project', $args );
-
-	}
-	
+	}	
 	
 	function change_title_text( $title ) {
 		$screen = get_current_screen();
@@ -135,47 +132,50 @@ class FS_CPT {
 	
 
 	// Register Custom Taxonomy
+
+	function tax_project() {
 	
-	function custom_taxonomy() {
-		
-		// project_type
 		$labels = array(
-			'name'                       => _x( 'Projects types', 'Taxonomy General Name', 'from-scratch' ),
+			'name'                       => _x( 'Project types', 'Taxonomy General Name', 'from-scratch' ),
 			'singular_name'              => _x( 'Project type', 'Taxonomy Singular Name', 'from-scratch' ),
-			'menu_name'                  => __( 'Project type', 'from-scratch' ),
-			'all_items'                  => __( 'All types', 'from-scratch' ),
-			'parent_item'                => __( 'Parent type', 'from-scratch' ),
-			'parent_item_colon'          => __( 'Parent type:', 'from-scratch' ),
-			'new_item_name'              => __( 'New type name', 'from-scratch' ),
-			'add_new_item'               => __( 'Add type Item', 'from-scratch' ),
-			'edit_item'                  => __( 'Edit type', 'from-scratch' ),
-			'update_item'                => __( 'Update type', 'from-scratch' ),
-			'view_item'                  => __( 'View type', 'from-scratch' ),
-			'separate_items_with_commas' => __( 'Separate types with commas', 'from-scratch' ),
-			'add_or_remove_items'        => __( 'Add or remove types', 'from-scratch' ),
+			'menu_name'                  => __( 'Project types', 'from-scratch' ),
+			'all_items'                  => __( 'All Items', 'from-scratch' ),
+			'parent_item'                => __( 'Parent Item', 'from-scratch' ),
+			'parent_item_colon'          => __( 'Parent Item:', 'from-scratch' ),
+			'new_item_name'              => __( 'New Item Name', 'from-scratch' ),
+			'add_new_item'               => __( 'Add New Item', 'from-scratch' ),
+			'edit_item'                  => __( 'Edit Item', 'from-scratch' ),
+			'update_item'                => __( 'Update Item', 'from-scratch' ),
+			'view_item'                  => __( 'View Item', 'from-scratch' ),
+			'separate_items_with_commas' => __( 'Separate items with commas', 'from-scratch' ),
+			'add_or_remove_items'        => __( 'Add or remove items', 'from-scratch' ),
 			'choose_from_most_used'      => __( 'Choose from the most used', 'from-scratch' ),
-			'popular_items'              => __( 'Popular types', 'from-scratch' ),
-			'search_items'               => __( 'Search types', 'from-scratch' ),
+			'popular_items'              => __( 'Popular Items', 'from-scratch' ),
+			'search_items'               => __( 'Search Items', 'from-scratch' ),
 			'not_found'                  => __( 'Not Found', 'from-scratch' ),
-			'no_terms'                   => __( 'No types', 'from-scratch' ),
-			'items_list'                 => __( 'Types list', 'from-scratch' ),
-			'items_list_navigation'      => __( 'Types list navigation', 'from-scratch' ),
+			'no_terms'                   => __( 'No items', 'from-scratch' ),
+			'items_list'                 => __( 'Items list', 'from-scratch' ),
+			'items_list_navigation'      => __( 'Items list navigation', 'from-scratch' ),
+		);
+		$rewrite = array(
+			'slug'                       => __('project-type', 'from-scratch'),
+			'with_front'                 => true,
+			'hierarchical'               => false,
 		);
 		$args = array(
 			'labels'                     => $labels,
-			'hierarchical'               => false,
+			'hierarchical'               => true,
 			'public'                     => true,
 			'show_ui'                    => true,
 			'show_admin_column'          => true,
 			'show_in_nav_menus'          => true,
-			'show_tagcloud'              => false,
-			'query_var'                  => 'project-type',
+			'show_tagcloud'              => true,
+			'rewrite'                    => $rewrite,
 			'show_in_rest'               => true,
 		);
-		register_taxonomy( 'project_type', array( 'project' ), $args );
-		
-	}
+		register_taxonomy( 'project-type', array( 'project' ), $args );
 	
+	}	
 	
 }
 
