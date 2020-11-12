f1b1d67c9d2116b643189dc6392008956c4e377c
Thomas Villain <atelier@anybodesign.com>
2020-11-12 14:17:15 +0100

    Updated style guide

--- template-parts/temp-styleguide.php
+++ template-parts/temp-styleguide.php
@@ -18,8 +18,20 @@
 					<p>Curabitur blandit tempus porttitor. <a href="#">Lorem ipsum dolor</a> sit amet, consectetur adipiscing elit. Aenean eu leo quam. <strong>Pellentesque ornare</strong> sem lacinia quam venenatis vestibulum.</p>
 
 					<div>Inside a div…</div>
-					
 					<span>Inside a span</span>
+					
+					<h2>H2 Title hierarchy lacinia bibendum nulla sed consectetur</h2>
+					<p>Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean lacinia bibendum nulla sed consectetur. Donec sed odio dui.</p>
+					<h3>H3 Donec id elit non mi porta gravida at eget metus</h3>
+					<p>Maecenas sed diam eget risus varius blandit sit amet non magna. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+					<h4>H4 Lorem ipsum dolor sit amet</h4>
+					<p>Consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Sed posuere consectetur est at lobortis. Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+					<h5>H5 Bibenbum lacinia bibendum nulla sed consectetur</h5>
+					<p>Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean lacinia bibendum nulla sed consectetur. Donec sed odio dui.</p>
+					<h6>H6 Donec id elit non mi porta gravida at eget metus</h6>
+					<p>Maecenas sed diam eget risus varius blandit sit amet non magna. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam quis risus eget urna mollis ornare vel eu leo.</p>					
+					
+					<hr>
 										
 					<h3>This is a section subtitle</h3>
 					
@@ -60,7 +72,8 @@
 						
 						<h4>Some content on a dark background</h4>
 						
-						<p>Vivamus sagittis lacus vel augue <span>laoreet (in a span)</span> rutrum faucibus dolor auctor. <strong>Vivamus sagittis lacus vel augue laoreet</strong> rutrum faucibus dolor auctor. Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Cras mattis consectetur purus sit amet fermentum. <em>Curabitur blandit tempus porttitor.</em></p>
+						<p class="text-intro">Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vestibulum id ligula porta felis euismod semper.</p>
+						<p>Vivamus sagittis <a href="#">lorem ipsum dolor</a> lacus vel augue <span>laoreet (in a span)</span> rutrum faucibus dolor auctor. <strong>Vivamus sagittis lacus vel augue laoreet</strong> rutrum faucibus dolor auctor. Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Cras mattis consectetur purus sit amet fermentum. <em>Curabitur blandit tempus porttitor.</em></p>
 						<ul>
 							<li>Toto</li>
 							<li>Lili</li>
@@ -70,78 +83,83 @@
 						
 					</div>
 					
-					<p>A code block:</p>
-					<pre>
-					    array(
-					        &nbsp;'name' => __( 'very light gray', 'themeLangDomain' ),
-					        &nbsp;'slug' => 'very-light-gray',
-					        &nbsp;'color' => '#e0e0e0',
-					    ),
-					</pre>
+					<hr>
 					
-					<p>Here is some <mark>inline</mark> code: <code>color: #444;</code> (CSS)</p>
-					
-					<h3>Forms</h3>
-					
-					<p>Sed posuere consectetur est at lobortis.</p>
-					
-					<form style="max-width: 50rem;">
-						<fieldset>
-							<legend>Fieldset Legend</legend>
+					<div style="display: flex; flex-wrap: wrap; justify-content: space-between;">
+						<div style="width: 100%; max-width: 45rem;">
+							<h3>Forms</h3>
 							
-							<div class="formfield-text">
-								<label for="inp_text">The Label</label>
-								<input type="text" id="inp_text" placeholder="A placeholder…">
-							</div>
-							
-							<div class="formfield-checkbox">
-								<input type="checkbox" id="inp_check">
-								<label for="inp_check">The Label</label>
-							</div>
-							
-							<div class="formfield-radio">
-								<input type="radio" id="inp_rad">
-								<label for="inp_rad">The Label</label>
-							</div>
-							
-							<div class="formfield-select">
-								<label for="inp_sel" class="screen-reader-text">The Label</label>
-								<div class="formfield-select--container">
-									<select id="inp_sel">
-										<option>Option 1</option>
-										<option>Option 2 with a very very long option text to show ellipsis</option>
-										<option>Option 3</option>
-										<option>Option 4</option>
-									</select>
+							<form style="max-width: 50rem;">
+								<fieldset>
+									<legend>Fieldset Legend</legend>
 									
-								</div>
-							</div>
-							
-							<input type="submit" class="action-btn" value="Send">
-							
-						</fieldset>
-					</form>
+									<div class="formfield-text">
+										<label for="inp_text">The Label</label>
+										<input type="text" id="inp_text" placeholder="with a placeholder…">
+									</div>
+									<div class="formfield-text">
+										<label for="inp_text2">The other Label</label>
+										<input type="text" id="inp_text2">
+									</div>
+									
+									<legend>Fieldset Legend</legend>
+									
+									<div class="formfield-checkbox">
+										<input type="checkbox" id="inp_check">
+										<label for="inp_check">The Label</label>
+									</div>
+									
+									<div class="formfield-radio">
+										<input type="radio" id="inp_rad">
+										<label for="inp_rad">The Label</label>
+									</div>
+									
+									<div class="formfield-select">
+										<label for="inp_sel" class="screen-reader-text">The Label</label>
+										<div class="formfield-select--container">
+											<select id="inp_sel">
+												<option>The select</option>
+												<option>Option 1</option>
+												<option>Option 2 with a very very long option text to show ellipsis</option>
+												<option>Option 3</option>
+												<option>Option 4</option>
+											</select>
+											
+										</div>
+									</div>
+									<div class="formfield-text">
+										<label for="inp_textarea">The text area</label>
+										<textarea id="inp_textarea"></textarea>
+									</div>
+									
+									<input type="submit" class="action-btn" value="Send">
+									
+								</fieldset>
+							</form>
+						</div>
+						
+						<div style="width: 100%; max-width: 47rem;">
+							<h3>Images, Players & Tables</h3>
+							<figure style="max-width: 50rem;">
+								<a href="#"><img src="http://placecorgi.com/500/340"></a>
+								<figcaption>
+									Place a Corgi
+								</figcaption>
+							</figure>
+							<figure style="max-width: 50rem;">
+								<a href="https://www.youtube.com/watch?v=PtMx316Vpvw">
+									<img src="https://i1.ytimg.com/vi/PtMx316Vpvw/mqdefault.jpg">
+								</a>
+								<figcaption>
+									Ride - Pulsar (vidéo)
+								</figcaption>
+							</figure>
+						</div>
+					</div>
+						
 					
-					
-					
-					<h3>Images, Players & Tables</h3>
-					
-					<figure style="max-width: 50rem;">
-						<a href="#"><img src="http://placecorgi.com/500/340"></a>
-						<figcaption>
-							Place a Corgi
-						</figcaption>
-					</figure>
-				
-					<figure style="max-width: 50rem;">
-						<a href="https://www.youtube.com/watch?v=PtMx316Vpvw">
-							<img src="https://i1.ytimg.com/vi/PtMx316Vpvw/mqdefault.jpg">
-						</a>
-						<figcaption>
-							Ride - Pulsar (vidéo)
-						</figcaption>
-					</figure>
-
+					<h3>Tables</h3>
+						
 					<div class="table-container">
 						<table width="100%">
 							<thead>
@@ -179,3 +197,16 @@
 							</tfoot>
 						</table>
 					</div>
+					
+					
+					<h3>Code</h3>
+					
+					<pre>
+					    array(
+					        &nbsp;'name' => __( 'very light gray', 'themeLangDomain' ),
+					        &nbsp;'slug' => 'very-light-gray',
+					        &nbsp;'color' => '#e0e0e0',
+					    ),
+					</pre>
+					
+					<p>Here is some <mark>inline</mark> code: <code>color: #444;</code> (CSS)</p>					
