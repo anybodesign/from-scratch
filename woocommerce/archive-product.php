ae20681bb131f79b3640f9b530a74afb3e1385b7
Thomas Villain <atelier@anybodesign.com>
2020-11-26 18:38:45 +0100

    Woocommerce templates

---
+++ woocommerce/archive-product.php
@@ -0,0 +1,56 @@
+<?php defined( 'ABSPATH' ) || exit;
+/**
+ * The Template for displaying product archives, including the main shop page which is a post type archive
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.4.0
+ */
+get_header( 'shop' ); ?>
+
+			<?php 
+				get_template_part( 'template-parts/page', 'banner' ); 
+			?>
+			
+			<div class="page-content">                       
+               
+				<?php
+				if ( woocommerce_product_loop() ) {
+					
+					woocommerce_product_loop_start();
+					
+					if ( wc_get_loop_prop( 'total' ) ) {
+						while ( have_posts() ) {
+							the_post();
+							do_action( 'woocommerce_shop_loop' );
+							wc_get_template_part( 'content', 'product' );
+						}
+					}
+					
+					woocommerce_product_loop_end();
+					/**
+					 * Hook: woocommerce_after_shop_loop.
+					 *
+					 * @hooked woocommerce_pagination - 10
+					 */
+					do_action( 'woocommerce_after_shop_loop' );
+				} else {
+					/**
+					 * Hook: woocommerce_no_products_found.
+					 *
+					 * @hooked wc_no_products_found - 10
+					 */
+					do_action( 'woocommerce_no_products_found' );
+				} ?>
+						
+			</div>
+
+<?php get_footer( 'shop' ); ?>
