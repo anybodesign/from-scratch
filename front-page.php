2aeadddde7b655a1931b2752e29d499fb40e6e01
Thomas Villain <atelier@anybodesign.com>
2021-01-12 18:47:36 +0100

    double loop fix

--- front-page.php
+++ front-page.php
@@ -13,12 +13,7 @@ get_header(); ?>
 
 				<div class="page-wrap">
 					
-					<?php while ( have_posts() ) : the_post(); ?>
-		
-						<?php get_template_part( 'template-parts/page', 'content' ); ?>
-		
-					<?php endwhile; ?>
-					
+					<?php get_template_part( 'template-parts/page', 'content' ); ?>					
 					
 					<?php 
 						
