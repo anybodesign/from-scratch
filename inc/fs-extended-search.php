43b5df7c7fb27385216a2b435f020ce6c5df05eb
Thomas Villain <atelier@anybodesign.com>
2021-01-12 09:06:48 +0100

    extended search fix
    thanks to oelita

--- inc/fs-extended-search.php
+++ inc/fs-extended-search.php
@@ -14,7 +14,7 @@ function cf_search_join( $join ) {
     global $wpdb;
 
     if ( is_search() ) {    
-        $join .=' LEFT JOIN '.$wpdb->postmeta. ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
+        $join .=' LEFT JOIN '.$wpdb->postmeta. ' as cfpm ON '. $wpdb->posts . '.ID = cfpm.post_id ';
     }
 
     return $join;
@@ -29,10 +29,10 @@ add_filter('posts_join', 'cf_search_join' );
 function cf_search_where( $where ) {
     global $pagenow, $wpdb;
 
-    if ( is_search() ) {
+    if ( is_search() ) {		
         $where = preg_replace(
             "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
-            "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
+            "(".$wpdb->posts.".post_title LIKE $1) OR (cfpm.meta_value LIKE $1)", $where );		
     }
 
     return $where;
