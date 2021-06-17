02168422581e3ea97dbe30d17ddca1019b971e89
Thomas Villain <atelier@anybodesign.com>
2021-06-17 16:37:49 +0200

    post meta and category dropdown output

--- index.php
+++ index.php
@@ -9,15 +9,47 @@
  * @since 1.0
  * @version 1.0
  */ 
+	if ( get_theme_mod('blog_sidebar') != true ) {
+		$sidebar = false;
+	} else {
+		$sidebar = true;
+	}
+	$cats = get_theme_mod('cat_dropdown');
+	
 get_header(); ?>
 
 				<?php get_template_part( 'template-parts/page', 'banner' ); ?>
 				
-				<div class="page-wrap has-sidebar">
+				<div class="page-wrap<?php if ($sidebar) { echo ' has-sidebar'; } ?>">
 					
 					<div class="page-content">
+							
+						<?php if ( have_posts() ) : // The Loop ?>
 						
-						<?php if ( have_posts() ) : // The Loop ?>		
+						<?php if ( $cats && ( is_home() || is_category() ) ) { ?>
+						<div class="page-filters">
+							<label for="cat">
+								<?php _e('Categories', 'from-scratch'); ?>
+							</label>
+							<div class="formfield-select--container">
+								<?php
+									$args = array(
+										'show_option_all' => __( 'All categories', 'from-scratch' ),
+									);
+									wp_dropdown_categories( $args ); 
+								?>
+							</div>
+							<script>
+								var siteUrl = '<?php esc_url(bloginfo('url')); ?>';
+								document.getElementById('cat').onchange = function(){
+									if( this.value !== '-1' ){
+										window.location=siteUrl +'/?cat='+this.value
+									}
+								}
+							</script>
+						</div>	
+						<?php } ?>
+							
 						<div id="posts_list" class="the-posts" >
 							<?php 
 								while ( have_posts() ) : the_post();
@@ -35,7 +67,7 @@ get_header(); ?>
 						?>
 					</div>
 					
-					<?php get_sidebar(); ?>
+					<?php if ($sidebar) { get_sidebar(); } ?>
 					
 				</div>
 
--- template-parts/post-meta.php
+++ template-parts/post-meta.php
@@ -9,18 +9,27 @@
  * @since 1.0
  * @version 1.0
  */
+	$date = get_theme_mod('meta_date', true);
+	$author = get_theme_mod('meta_author', true);
+	$cat = get_theme_mod('meta_category', true);
 ?>
-
 							<div class="post-meta">
+								
+								<?php if ( $date != false || $author != false || $cat != false ) { ?>
 								<p class="meta-infos">
-									<?php esc_html_e( 'Posted on&nbsp;', 'from-scratch' ); ?><?php echo the_time( get_option('date_format') ); ?>
-									<?php if ( get_theme_mod('meta_author') != false ) {
+									<?php esc_html_e( 'Posted ', 'from-scratch' ); ?>
+									<?php if ( $date != false ) { 
+										esc_html_e( 'on&nbsp;', 'from-scratch' ); 
+										echo the_time( get_option('date_format') ); 
+									} ?>
+									<?php if ( $author != false ) {
 										esc_html_e( 'by&nbsp;', 'from-scratch' ); the_author(); 
 									} ?>
-									<?php if ( get_theme_mod('meta_category') != false ) {
+									<?php if ( $cat != false ) {
 										esc_html_e( 'in&nbsp;', 'from-scratch' ); the_category(', '); 
 									} ?>
 								</p>
+								<?php } ?>
 								
 								<?php $comment = get_comments_number(); if ( $comment > 0 ) : ?>
 								<p class="meta-comments">
