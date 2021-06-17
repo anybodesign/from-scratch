afd66d87be77bd9cfe7f24146a2bcdb3b5ee4780
Thomas Villain <atelier@anybodesign.com>
2021-06-17 16:38:05 +0200

    burger button enhanced

--- template-parts/header-nav.php
+++ template-parts/header-nav.php
@@ -4,7 +4,11 @@
 			
 			<?php if ( has_nav_menu( 'main_menu' ) ) : ?>
 			<nav class="site-nav" role="navigation" aria-label="<?php esc_html_e('Main Menu', 'from-scratch'); ?>" id="site_nav">
-				<button class="burger" id="menu_toggle" type="button"><?php esc_html_e('Menu', 'from-scratch'); ?><span></span></button>
+				<button class="burger" id="menu_toggle" type="button">
+					<span>
+						<span class="burger-title"><?php esc_html_e('Menu', 'from-scratch'); ?></span>
+					</span>
+				</button>
 				<?php wp_nav_menu( array(
 					'theme_location'	=> 	'main_menu',
 					'menu_class'		=>	'main-menu',
