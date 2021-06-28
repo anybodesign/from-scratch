f72acdb5f486343b14433fd5d6aa4e193cfe23d6
Thomas Villain <atelier@anybodesign.com>
2021-06-28 17:03:55 +0200

    high contrast JS cleaning

--- js/high-contrast.js
+++ js/high-contrast.js
@@ -1,59 +1,12 @@
 jQuery(document).ready(function($) {
 	
-	
 // Contrast
 
-// https://stackoverflow.com/questions/19401633/how-to-fire-an-event-on-class-change-using-jquery
-
-var $div = $("body");
-var observer = new MutationObserver(function(mutations) {
-
-	mutations.forEach(function(mutation) {
-    
-    if (mutation.attributeName === "class") {
-
-		var attributeValue = $(mutation.target).prop(mutation.attributeName);
-      
-			if ( attributeValue === 'highcontrast' ) {
-				
-				$('.main-title img').attr('src','img/logo-black.svg');
-				$('.infographie').attr('src','img/infographie-nb.jpg');
-				$('.next-btn').attr('src','img/slider-btn-black.svg');
-			
-			} else if ( attributeValue === 'highcontrast fixed-nav' ) {
-				
-				$('.main-title img').attr('src','img/logo-white.svg');
-			
-			} else {
-				
-				$('.main-title img').attr('src','img/logo-ux-a11y.svg');
-				$('.infographie').attr('src','img/infographie-color.jpg'); 
-				$('.next-btn').attr('src','img/slider-btn.svg');
-			} 
-			
-			console.log("Class attribute changed to:", attributeValue);
-	}
-
-
-    
-	});
-});
-
-
-
-
-observer.observe(
-	$div[0], {attributes: true}
-);
-
-
     // Check (onLoad) if the cookie is there and set the class if it is
- 
 
     if ($.cookie('highcontrast') === "yes") {
         $("body").addClass("high-contrast");
     }
-    
     
     // When the element is clicked
    
@@ -87,7 +40,5 @@ observer.observe(
             });
         }
     });	
-	
-	
 
 });
