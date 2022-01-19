ce88b8e27db82f37bbaadab31de48ae0dad544b5
Thomas Villain <atelier@anybodesign.com>
2022-01-19 10:08:15 +0100

    esc fix

--- template-parts/footer-back2top.php
+++ template-parts/footer-back2top.php
@@ -1,5 +1,5 @@
 <?php if ( !defined('ABSPATH') ) die(); ?>
 			
-			<button id="back2top" title="<?php esc_html_e('Back to top','from-scratch'); ?>">
+			<button id="back2top" title="<?php esc_attr_e('Back to top','from-scratch'); ?>">
 				<img src="<?php echo FS_THEME_URL; ?>/img/ui/back-to-top.svg" alt="">
 			</button>	
