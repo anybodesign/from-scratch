b51f41869d9ef8c09560109543a23608a8dbfc27
Thomas Villain <atelier@anybodesign.com>
2022-09-20 11:15:01 +0000

    Header logo height

--- template-parts/header-brand-logo.php
+++ template-parts/header-brand-logo.php
@@ -14,13 +14,13 @@
 						$logo_h2 = get_theme_mod('site_logo_mobile_height');
 						if ( $logo_h2 ) { $height2 = $logo_h2/10; };
 					?>
-					<img class="logo-mobile" src="<?php echo esc_url(get_theme_mod('site_logo_mobile', 'none')); ?>" alt=""<?php if ($logo_h2) { echo ' style="max-height:'.$height2.'rem"'; } ?>>
+					<img class="logo-mobile" src="<?php echo esc_url(get_theme_mod('site_logo_mobile', 'none')); ?>" alt=""<?php if ($logo_h2) { echo ' style="height:'.$height2.'rem"'; } ?>>
 					<?php } ?>
 					<?php if ( get_theme_mod('site_logo') ) { 
 						$logo_h1 = get_theme_mod('site_logo_height');
 						if ( $logo_h1 ) { $height1 = $logo_h1/10; };
 					?>
-					<img class="logo<?php if ( get_theme_mod('site_logo_mobile') ) { echo esc_attr(' has-mobile-logo'); } ?>" src="<?php echo esc_url(get_theme_mod('site_logo', 'none')); ?>" alt=""<?php if ($logo_h1) { echo ' style="max-height:'.$height1.'rem"'; } ?>>
+					<img class="logo<?php if ( get_theme_mod('site_logo_mobile') ) { echo esc_attr(' has-mobile-logo'); } ?>" src="<?php echo esc_url(get_theme_mod('site_logo', 'none')); ?>" alt=""<?php if ($logo_h1) { echo ' style="height:'.$height1.'rem"'; } ?>>
 					<?php } ?>					
 					
 					<span class="site-name<?php if (get_theme_mod( 'hide_sitetitle' )) { echo esc_attr(' a11y-hidden'); } ?>"><?php echo esc_html(bloginfo('name')); ?></span>
