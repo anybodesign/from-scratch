6ed13fc596441fe9bbcff3b1a61adb08df791f15
Thomas Villain <atelier@anybodesign.com>
2023-01-19 12:21:51 +0100

    page banner template part

--- template-parts/page-banner.php
+++ template-parts/page-banner.php
@@ -9,30 +9,20 @@
  * @since 1.0
  * @version 1.0
  */
- 	$has_bg = get_theme_mod('has_bg'); 
+	$has_bg = get_theme_mod('has_bg');
+	$in_banner = get_theme_mod('child_pages') == 'banner';
+	$show_banner_desc = get_theme_mod('show_desc'); 
 ?>
 					<div class="page-banner"<?php if ( $has_bg ) { fs_bg_img(); } ?>>
 						<div class="inner">
-						
-							<h1 class="page-title">
-							<?php 	
-								if ( is_home() && ! is_front_page() ) {
-									single_post_title();
-								} else if ( is_archive() ) {
-									the_archive_title();
-								} else if ( is_search() ) {
-									printf( esc_html__( 'Search Results for: %s', 'from-scratch' ), '<span class="search-term">' . get_search_query() . '</span>' );
-								} else if ( is_404() ) {
-									esc_html_e( 'Oops! That page can&rsquo;t be found', 'from-scratch' );
-								} else {
-									the_title(); 
-								}
-							?>
-							</h1>
-							
 							<?php 
-								get_template_part( 'template-parts/page-banner', 'subnav' ); 
-								get_template_part( 'template-parts/page-banner', 'desc' ); 
+								get_template_part( 'template-parts/page-banner', 'title' );
+								if ($in_banner) { 
+									get_template_part( 'template-parts/page-banner', 'subnav' );
+								} 
+								if ($show_banner_desc) {
+									get_template_part( 'template-parts/page-banner', 'desc' );
+								}
 							?>
 						</div>
 					</div>
