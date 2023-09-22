72f664eede4f239d303196e1e6ea437481fe93e4
Thomas Villain <atelier@anybodesign.com>
2023-09-22 09:31:44 +0200

    Share on X

--- template-parts/post-share.php
+++ template-parts/post-share.php
@@ -9,8 +9,8 @@
 									<li><a target="_blank" rel="nofollow" title="<?php esc_attr_e('Share on Facebook', 'from-scratch'); ?>" href="https://www.facebook.com/sharer/sharer.php?t=<?php the_title(); ?>&amp;u=<?php the_permalink(); ?>">
 										<img src="<?php echo FS_THEME_URL; ?>/img/icons/share/facebook.svg" alt="Facebook">
 									</a></li>
-									<li><a target="_blank" rel="nofollow" title="<?php esc_attr_e('Share on Twitter', 'from-scratch'); ?>" href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&amp;url=<?php the_permalink(); ?>">
-										<img src="<?php echo FS_THEME_URL; ?>/img/icons/share/twitter.svg" alt="Twitter">
+									<li><a target="_blank" rel="nofollow" title="<?php esc_attr_e('Share on X', 'from-scratch'); ?>" href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&amp;url=<?php the_permalink(); ?>">
+										<img src="<?php echo FS_THEME_URL; ?>/img/icons/share/twitter-x.svg" alt="X">
 									</a></li>
 									<li><a target="_blank" rel="nofollow" title="<?php esc_attr_e('Share on LinkedIn', 'from-scratch'); ?>" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>&title=<?php the_title(); ?>&summary=<?php echo fs_share_excerpt(124, $id); ?>&source=<?php echo esc_html(bloginfo('name')); ?>">
 										<img src="<?php echo FS_THEME_URL; ?>/img/icons/share/linkedin.svg" alt="LinkedIn">
