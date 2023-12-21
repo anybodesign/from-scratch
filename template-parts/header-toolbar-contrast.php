2039fe35dcb2e95f088872f3a819a56e2c1d3973
Thomas Villain <atelier@anybodesign.com>
2023-12-21 15:39:12 +0100

    i18n fix

--- template-parts/header-toolbar-contrast.php
+++ template-parts/header-toolbar-contrast.php
@@ -1,11 +1,11 @@
 <?php if ( !defined('ABSPATH') ) die(); ?>
 			
 					<div class="contrast-switch">
-						<button class="toggle-highcontrast" type="button" title="<?php esc_attr_e('Toggle high contrast'); ?>">
+						<button class="toggle-highcontrast" type="button" title="<?php esc_attr_e('Toggle high contrast', 'from-scratch'); ?>">
 							<img src="<?php echo FS_THEME_URL; ?>/img/ui/contrast-on.svg" alt="">
 							<span><?php esc_html_e('High contrast', 'from-scratch'); ?></span>
 						</button>
-						<button class="toggle-remove" type="button" title="<?php esc_attr_e('Restore contrast'); ?>">
+						<button class="toggle-remove" type="button" title="<?php esc_attr_e('Restore contrast', 'from-scratch'); ?>">
 							<img src="<?php echo FS_THEME_URL; ?>/img/ui/contrast-off.svg" alt="">
 							<span><?php esc_html_e('Restore contrast', 'from-scratch'); ?></span>
 						</button>
