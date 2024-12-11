778361e97d8b547a667417035194ca32fcdbae9d
Thomas Villain <atelier@anybodesign.com>
2024-12-11 13:25:56 +0100

    Text domain fix

--- template-parts/header-toolbar-contrast.php
+++ template-parts/header-toolbar-contrast.php
@@ -3,6 +3,6 @@
 					<div class="contrast-switch">
 						<button class="toggle-contrast" type="button" title="<?php esc_attr_e('Toggle high contrast', 'from-scratch'); ?>">
 							<img class="icon" src="<?php echo FS_THEME_URL; ?>/img/ui/contrast-on.svg" alt="">
-							<span class="text"><?php esc_html_e('High contrast', 'sneci'); ?></span>
+							<span class="text"><?php esc_html_e('High contrast', 'from-scratch'); ?></span>
 						</button>
 					</div>
